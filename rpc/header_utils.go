7596fad9ec7ea873d5f20592a1f27d1bb91bce0a
crackcomm <crackcomm@gmail.com>
2018-03-02 05:08:51 +0100

    feat(*): string debug for rpc.Header

--- rpc/header_utils.go
+++ rpc/header_utils.go
@@ -1,6 +1,7 @@
 package rpc
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -15,4 +16,15 @@ func ReadHeader(conn io.Reader) (hdr *Header, err error) {
 	hdr = new(Header)
 	hdr.Init(buf, 0)
 	return
+}
+
+// String - Formats header as debug string.
+func (hdr *Header) String() string {
+	return fmt.Sprintf("[ compression=%d, bitflags=%d, session=%d, size=%d, checksum=%d, meta=%d ]",
+		hdr.Compression(),
+		hdr.Bitflags(),
+		hdr.Session(),
+		hdr.Size(),
+		hdr.Checksum(),
+		hdr.Meta())
 }
