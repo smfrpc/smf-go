7dba4e57e1c1a573dc0ae8bcf500dc893594888e
crackcomm <crackcomm@gmail.com>
2018-03-02 05:18:46 +0100

    feat(*): method to build response header

--- rpc/header_utils.go
+++ rpc/header_utils.go
@@ -3,6 +3,11 @@ package rpc
 import (
 	"fmt"
 	"io"
+	"math"
+
+	"github.com/cespare/xxhash"
+
+	flatbuffers "github.com/google/flatbuffers/go"
 )
 
 // ReadHeader - Reads smf RPC header from connection reader.
@@ -16,6 +21,25 @@ func ReadHeader(conn io.Reader) (hdr *Header, err error) {
 	hdr = new(Header)
 	hdr.Init(buf, 0)
 	return
+}
+
+// BuildHeader - Builds response header from response bytes and request header.
+func BuildHeader(req *Header, resp []byte, status uint32) []byte {
+	builder := flatbuffers.NewBuilder(20) // [1]
+	res := CreateHeader(builder,
+		0,                                         // compression int8,
+		0,                                         // bitflags int8,
+		req.Session(),                             // session uint16,
+		uint32(len(resp)),                         // size uint32,
+		uint32(math.MaxUint32&xxhash.Sum64(resp)), // checksum uint32,
+		status, //	meta uint32
+	)
+	builder.Finish(res)
+	// TODO(crackcomm): builder prepends 4 bytes
+	// the header is the last 16 bytes of message
+	// so I did [^1] 20 bytes allocation anyway
+	// I have no idea why it does that
+	return builder.FinishedBytes()[4:]
 }
 
 // String - Formats header as debug string.
